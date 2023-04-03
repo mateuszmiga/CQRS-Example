eea44b472cad160ae560081094400000e7b7d8bb
Mateusz Miga <mateusz.miga11@gmail.com>
2023-04-03 20:08:10 +0200

    validation

---
+++ CQRS.Application/User/Commands/CreateUser/CreateUserCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Application.User.Commands.CreateUser
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            this.RuleFor(u => u.Id).NotEmpty();
+        }
+    }
+}
